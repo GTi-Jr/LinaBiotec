c663c9cf14f281683acb3e47c11a564a3d197d0a
adolfoggc <adolfo.ggc@gmail.com>
2017-02-26 18:26:25 -0300

    correções menores

--- wp-content/themes/twentyseventeen/front-page.php
+++ wp-content/themes/twentyseventeen/front-page.php
@@ -19,9 +19,7 @@ get_header(); ?>
 		<?php // Show the selected frontpage content.
 		if ( have_posts() ) :
 			while ( have_posts() ) : the_post();
-				print "hueheuhuehuehe1";
 				get_template_part( 'template-parts/page/content', 'front-page' );
-				print "hueheuhuehuehe";
 			endwhile;
 		else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
 			get_template_part( 'template-parts/post/content', 'none' );
